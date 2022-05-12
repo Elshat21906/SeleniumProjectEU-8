88fe620a3902866100a5b539ad2a3a179676c2cd
Elshat <z90602190@hotmail.com>
2022-05-12 18:29:43 +0300

    Day04:findElement_CheckBox_radioButton

--- src/test/java/com/cydeo/tests/day4_FindElement_CheckBox_RadioButton/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_FindElement_CheckBox_RadioButton/T1_xpath_cssSelector_practice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day4_FindElement_CheckBox_RadioButton;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T1_xpath_cssSelector_practice {
     public static void main(String[] args) {
@@ -14,11 +16,18 @@ public class T1_xpath_cssSelector_practice {
         t1.manage().window().maximize();
 
         // 2. Go to https://practice.cydeo.com/forgot_password
-        t1.get("https://practice.cydeo.com/forgot password");
+        t1.get("https://practice.cydeo.com/forgot_password");
 
         // 3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
 
-        //a. “Home” link
+        //a “Home” link
+        // Locate homeLink using cssSelector
+        WebElement homeLink1_ex1 = t1.findElement(By.cssSelector("a[class='nav-link']"));
+
+        // Locate homeLink using cssSelector syntax #2
+        WebElement homeLink1_ex2 = t1.findElement(By.cssSelector("a.nav-link"));
+
+
         //b. “Forgot password” header c. “E-mail” text
         //d. E-mail input box
         //e. “Retrieve password” button f. “Powered by Cydeo text
