8cbbf58a188923ccba87a8b2e6e144eeef59b997
Elshat <z90602190@hotmail.com>
2022-05-12 18:08:52 +0300

    Day04:findElement_CheckBox_radioButton

---
+++ src/test/java/com/cydeo/tests/FindElement_CheckBox_RadioButton/T1_xpath_cssSelector_practice.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.FindElement_CheckBox_RadioButton;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class T1_xpath_cssSelector_practice {
+    public static void main(String[] args) {
+
+
+        // TC #1: XPATH and cssSelector Practices
+
+        //  1. Open Chrome browser
+        WebDriver t1 = WebDriverFactory.getDriver("chrome");
+        t1.manage().window().maximize();
+
+        // 2. Go to https://practice.cydeo.com/forgot_password
+        t1.get("https://practice.cydeo.com/forgot password");
+
+        // 3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
+
+        //a. “Home” link
+        //b. “Forgot password” header c. “E-mail” text
+        //d. E-mail input box
+        //e. “Retrieve password” button f. “Powered by Cydeo text
+
+        //4. Verify all web elements are displayed.
+        // First solve the task with using cssSelector only. Try to create 2 different
+        // cssSelector if possible
+        // Then solve the task using XPATH only. Try to create 2 different XPATH locator if possible
+    }
+}
+
