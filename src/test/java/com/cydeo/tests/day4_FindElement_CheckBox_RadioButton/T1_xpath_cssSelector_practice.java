07a07fae6c919840b02834c34a0e832fc9b161c6
Elshat <z90602190@hotmail.com>
2022-05-13 13:27:59 +0300

    Day04:findElement_CheckBox_radioButton

--- src/test/java/com/cydeo/tests/day4_FindElement_CheckBox_RadioButton/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_FindElement_CheckBox_RadioButton/T1_xpath_cssSelector_practice.java
@@ -48,14 +48,30 @@ public class T1_xpath_cssSelector_practice {
         //d. E-mail input box
         WebElement inputBox1 = t1.findElement(By.xpath("//input[@name='email']"));
         WebElement inputBox2 = t1.findElement(By.xpath("//input[@type='text']"));
-
         //Locate input using xpath contains method
         //tagName[contains(@attribute,'value')]
         WebElement inputBox3 = t1.findElement(By.xpath("//input[contains(@pattern,'[a-z')]"));
 
-        //e. “Retrieve password” button f. “Powered by Cydeo text
+        //e. “Retrieve password” button
+        WebElement RetrievePassword1 = t1.findElement(By.xpath("//button[@id='form_submit']"));
+        WebElement RetrievePassword2 = t1.findElement(By.xpath("//button[@class='radius']"));
+        WebElement RetrievePassword3 = t1.findElement(By.xpath("//button[@type='submit']"));
+
+        //f. “Powered by Cydeo text
+        WebElement powerByCydeoText = t1.findElement(By.xpath("//div[@style='text-align: center;']"));
 
         //4. Verify all web elements are displayed.
+        System.out.println(homeLink1_ex1.isDisplayed());
+        System.out.println(homeLink1_ex2.isDisplayed());
+        System.out.println(homeLink1_ex3.isDisplayed());
+
+        System.out.println("ForgotPasswordHeader = " + ForgotPasswordHeader.isDisplayed());
+        System.out.println("header = " + header.isDisplayed());
+        System.out.println("emailLabel = " + emailLabel.isDisplayed());
+        System.out.println("inputBox1 = " + inputBox1.isDisplayed());
+        System.out.println("RetrievePassword1 = " + RetrievePassword1.isDisplayed());
+        System.out.println("powerByCydeoText = " + powerByCydeoText.isDisplayed());
+
         // First solve the task with using cssSelector only. Try to create 2 different
         // cssSelector if possible
         // Then solve the task using XPATH only. Try to create 2 different XPATH locator if possible
