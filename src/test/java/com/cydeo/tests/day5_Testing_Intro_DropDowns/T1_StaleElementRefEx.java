c0d2ad80f02dc5a6011741b0d7b6253c43f8cb30
Elshat <z90602190@hotmail.com>
2022-05-18 19:58:45 +0300

    day5:Task1 Stale Element RefEx

--- src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/T1_StaleElementRefEx.java
@@ -2,13 +2,14 @@ package com.cydeo.tests.day5_Testing_Intro_DropDowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class T1_StaleElementRefEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #1: StaleElementReferenceException handling
         //1. Open Chrome browser
@@ -20,13 +21,28 @@ public class T1_StaleElementRefEx {
         driver.get("https://practice.cydeo.com/add_remove_elements/");
 
         //3. Click to “Add Element” button
+        //button[@onclick='addElement()']
         WebElement addElement = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+        Thread.sleep(2000);
         addElement.click();
 
-        //button[@onclick='addElement()']
         //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
         //5. Click to “Delete” button.
+        deleteButton.click();
+
         //6. Verify “Delete” button is NOT displayed after clicking.
+        try{
+            System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
+        }catch(StaleElementReferenceException e){
+
+            System.out.println("-->StaleElementReferenceException exception is thrown");
+            System.out.println("-->This means the web element is completely deleted from the page");
+            System.out.println("deleteButton.isDisplayed() = false");
+        }
 
 
 
