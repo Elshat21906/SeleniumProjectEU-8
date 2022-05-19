012ed86558fb54c5c59f77bddbafd6d935203136
Elshat <z90602190@hotmail.com>
2022-05-19 13:43:02 +0300

    day5:Task3 T3_RadioButton_Cont

--- src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/T3_RadioButton_Cont.java
+++ src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/T3_RadioButton_Cont.java
@@ -9,35 +9,42 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T3_RadioButton_Cont {
+
     public static void main(String[] args) {
 
         //TC #2: Radiobutton handling
         //1. Open Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //2. Go to https://practice.cydeo.com/radio_buttons
-        driver.get(" https://practice.cydeo.com/radio_buttons");
+        driver.get("https://practice.cydeo.com/radio_buttons");
 
         //Locate name='sports' radio buttons and store them in a List of Web Element
-        List<WebElement> sportRadioButton = driver.findElements(By.name("sport"));
+
+        clickAndVerifyRadioButton(driver, "sport", "hockey" );
+
+        clickAndVerifyRadioButton(driver, "sport", "football");
+
+        clickAndVerifyRadioButton(driver, "color", "yellow");
+
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue){
+        List<WebElement> radioButtons = driver.findElements(By.name(nameAttribute));
 
         //Loop through the List of WebElement and select matching result "hockey"
-        for (WebElement each : sportRadioButton) {
+        for (WebElement each : radioButtons) {
+            String eachId = each.getAttribute("id");
 
-            String eachID = each.getAttribute("id");
-            System.out.println("eachID = " + eachID);
+            if (eachId.equals(idValue)){
 
-            if(eachID.equals("hockey")){
                 each.click();
-                System.out.println("Hockey is selected: " + each.isSelected());
+                System.out.println( eachId + " is selected : " + each.isSelected());
                 break;
             }
         }
 
-
-
-
     }
+
 }
