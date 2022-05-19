44373af2e74de40ea40a179a734cdb99f9ab1e95
Elshat <z90602190@hotmail.com>
2022-05-19 16:00:31 +0300

    day5:TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/TestNG_Intro.java
@@ -1,9 +1,26 @@
 package com.cydeo.tests.day5_Testing_Intro_DropDowns;
 
-
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class TestNG_Intro {
+    @Test (priority = 1)// yellow means it can run without main method -- imported
+    public void test1(){
+        System.out.println(" Test 1 is running ...");
+
+        // //ASSERT EQUALS: compare 2 of the same things
+               String actual = "apple";
+               String expected = "apple";
+        Assert.assertEquals(actual,expected);
+    }
+
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println(" Test 2 is running ...");
+    }
+
+
 
     @BeforeClass
     public void setupMethod(){
@@ -15,24 +32,14 @@ public class TestNG_Intro {
         System.out.println(" ---> After Class is running");
     }
 
-
     @BeforeMethod
     public void setUpMethod(){
         System.out.println(" ---> Before Method is running ... ");
     }
+
     @AfterMethod
     public void tearDownMethod(){
         System.out.println(" ---> After Method is running ...");
-    }
-
-    @Test (priority = 2)// yellow means it can run without main method -- imported
-    public void test1(){
-        System.out.println(" Test 1 is running ...");
-    }
-
-    @Test (priority = 1)
-    public void test2(){
-        System.out.println(" Test 2 is running ...");
     }
 
 }
