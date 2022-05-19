97864bedafeaf83f691ae84a4378270975c3d98a
Elshat <z90602190@hotmail.com>
2022-05-19 14:40:21 +0300

    day5:TestNG_Intro

---
+++ src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/TestNG_Intro.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day5_Testing_Intro_DropDowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+    @Test // yellow means it can run without main method -- imported
+    public void test1(){
+        System.out.println(" Test 1 is running ...");
+
+    }
+
+}
