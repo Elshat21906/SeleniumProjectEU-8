ae3910182e9feff54045bda848609bc39317c271
Elshat <z90602190@hotmail.com>
2022-05-19 15:50:50 +0300

    day5:TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_Testing_Intro_DropDowns/TestNG_Intro.java
@@ -1,10 +1,20 @@
 package com.cydeo.tests.day5_Testing_Intro_DropDowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println(" ---> Before Class is running ");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println(" ---> After Class is running");
+    }
+
 
     @BeforeMethod
     public void setUpMethod(){
@@ -15,12 +25,12 @@ public class TestNG_Intro {
         System.out.println(" ---> After Method is running ...");
     }
 
-    @Test // yellow means it can run without main method -- imported
+    @Test (priority = 2)// yellow means it can run without main method -- imported
     public void test1(){
         System.out.println(" Test 1 is running ...");
     }
 
-    @Test
+    @Test (priority = 1)
     public void test2(){
         System.out.println(" Test 2 is running ...");
     }
