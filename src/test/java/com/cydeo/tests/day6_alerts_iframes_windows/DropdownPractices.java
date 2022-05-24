eb3205e128f8a693943f62a022735a369639edcb
Elshat <z90602190@hotmail.com>
2022-05-24 13:13:56 +0300

    day6:DropdownPractices

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,23 +5,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
 
-    @Test
+    public WebDriver driver;
 
-    public void dropdown_task5() throws InterruptedException {
-        //TC #5: Selecting state from State dropdown and verifying result
+    @BeforeMethod
+
+    public void setupMethod(){
         // 1. Open Chrome browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //2. Go to http://practice.cybertekschool.com/dropdown
         driver.get("http://practice.cybertekschool.com/dropdown");
+    }
+
+    @Test
+
+    public void dropdown_task5() throws InterruptedException {
+        //TC #5: Selecting state from State dropdown and verifying result
+
 
         // We located the dropdown and it is ready to use
         Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
@@ -47,6 +56,11 @@ public class DropdownPractices {
 
     }
 
+    @Test
+    public void dropdown_task6(){
+
+    }
+
 }
 /*
 TC #5: Selecting state from State dropdown and verifying result 1. Open Chrome browser
@@ -56,4 +70,14 @@ TC #5: Selecting state from State dropdown and verifying result 1. Open Chrome b
 5. Select California
 6. Verify final selected option is California.
 Use all Select options. (visible text, value, index)
+ */
+
+/*
+TC #6: Selecting date on dropdown and verifying
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/dropdown
+3. Select “December 1st, 1923” and verify it is selected.
+   Select year using : visible text
+   Select month using : value attribute
+   Select day using : index number
  */
