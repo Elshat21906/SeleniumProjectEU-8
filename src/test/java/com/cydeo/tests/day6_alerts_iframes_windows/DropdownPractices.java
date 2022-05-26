06e4bf9a5215ced47864d1ec25a803f50d73cf16
Elshat <z90602190@hotmail.com>
2022-05-26 09:27:46 +0300

    day6:DropdownPractices

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -59,6 +59,14 @@ public class DropdownPractices {
     @Test
     public void dropdown_task6(){
 
+        // Select “December 1st, 1923” and verify it is selected.
+        Select yearDropdown = new Select(driver.findElement(By.xpath("")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("")));
+        Select dataDropdown = new Select(driver.findElement(By.xpath("")));
+        // Select year using : visible text
+        // Select month using : value attribute
+        // Select day using : index number
+
     }
 
 }
