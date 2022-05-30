11c0c60050afb647e025be782f7548eac9e0d7a6
Elshat <z90602190@hotmail.com>
2022-05-30 22:18:41 +0300

    day8_properties_config_reader

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WebTable_Order_Verify {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+
+    public void order_name_verify_test(){
+
+        //TC #1: Web table practice
+        //1. Goto:https://practice.cydeo.com/web-tables
+        driver.get("https://practice.cydeo.com/web-tables");
+
+        //2. VerifyBob’s name is listed as expected.
+
+        //Expected: “Bob Martin”
+        //3. VerifyBobMartin’s order date is as expected: 12/31/2021
+
+    }
+
+}
+/*
+TC #1: Web table practice
+1. Goto:https://practice.cydeo.com/web-tables
+2. VerifyBob’s name is listed as expected.
+Expected: “Bob Martin”
+3. VerifyBobMartin’s order date is as expected: 12/31/2021
+ */
