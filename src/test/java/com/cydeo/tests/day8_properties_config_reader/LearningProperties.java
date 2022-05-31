9c0afc1c810e0a880cac5a64b3ed7ffb3e244691
Elshat <z90602190@hotmail.com>
2022-05-31 15:28:44 +0300

    day8_properties_config_reader

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
@@ -7,6 +7,10 @@ public class LearningProperties {
     @Test
 
     public void java_properties_reading_test(){
+        //key = value
+        //os.name = Mac OS X
+        //user.name = elshata
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
     }
 }
