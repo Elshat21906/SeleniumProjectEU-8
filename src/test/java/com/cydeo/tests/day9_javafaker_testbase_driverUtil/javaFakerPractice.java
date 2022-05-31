fef6d9296243523065cd011e89ca0e8433806b06
Elshat <z90602190@hotmail.com>
2022-05-31 20:22:02 +0300

    day9_javaFaker_testBase_driverUtil

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/javaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/javaFakerPractice.java
@@ -1,11 +1,17 @@
 package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
 public class javaFakerPractice {
 
     @Test
     public void test1(){
+
+        // Create Faker object to reach methods
+        Faker faker = new Faker();
+
+        System.out.println("faker.name().firstName() = " + faker.name().firstName());
 
     }
 }
