79adba6a8218e31b6ec512e829cde355e5626e49
Elshat <z90602190@hotmail.com>
2022-05-31 20:36:37 +0300

    day9_javaFaker_testBase_driverUtil

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/javaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/javaFakerPractice.java
@@ -12,6 +12,27 @@ public class javaFakerPractice {
         Faker faker = new Faker();
 
         System.out.println("faker.name().firstName() = " + faker.name().firstName());
+        System.out.println("faker.name().lastName() = " + faker.name().lastName());
+        System.out.println("faker.name().fullName() = " + faker.name().fullName());
+
+        //Numerify() method will generate random numbers in the format we want to get
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("###-###-####"));
+
+        System.out.println("faker.numerify(\"312-###-####\") = "
+                + faker.numerify("312-###-####"));
+
+        //letterify() method will return random letters where we pass "?"
+        System.out.println("faker.letterify(\"???-????\") = "
+                + faker.letterify("???-????"));
+
+        System.out.println("faker.bothify(\"##?#-##?#-#?#?#-##??\") = "
+                + faker.bothify("##?#-##?#-#?#?#-##??"));
+
+        System.out.println("faker.finance().creditCard() = "
+                + faker.finance().creditCard().replaceAll("-", ""));
+
+        System.out.println("faker.chuckNorris().fact() = "
+                + faker.chuckNorris().fact().replaceAll("Chuck Norris", "Muhtar"));
 
     }
 }
