39476c87d3a5311b1437a9a307d52b515b8137f9
Elshat <z90602190@hotmail.com>
2022-05-31 11:14:11 +0300

    day8_properties_config_reader

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.WebTableUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,7 +17,7 @@ public class T1_WebTable_Order_Verify {
 
     @BeforeMethod
 
-    public void setupMethod(){
+    public void setupMethod() {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -24,7 +25,7 @@ public class T1_WebTable_Order_Verify {
 
     @Test
 
-    public void order_name_verify_test(){
+    public void order_name_verify_test() {
 
         //TC #1: Web table practice
         //1. Goto:https://practice.cydeo.com/web-tables
@@ -32,22 +33,41 @@ public class T1_WebTable_Order_Verify {
 
         // Locate the cell that has Bob Martin text in it
         WebElement bobMartinCell = driver.findElement(By.xpath("//table[@class='SampleTable']//td[.='Bob Martin']"));
-
         //2. VerifyBob’s name is listed as expected.
         //Expected: “Bob Martin”
         String expectedName = "Bob Martin";
         String actualName = bobMartinCell.getText();
-        Assert.assertEquals(actualName,expectedName);
+        Assert.assertEquals(actualName, expectedName);
 
-        //3. VerifyBobMartin’s order date is as expected: 12/31/2021
+        //3. VerifyBobMartin’s order date is as
+        // expected: 12/31/2021
+        // alternative locator to bobMartinDateCell = "//td[.='Bob Martin']/../td[5]"
         WebElement bobMartinDateCell = driver.findElement(By.xpath("//table[@class='SampleTable']//td[.='Bob Martin']/following-sibling::td[3]"));
         String expectedBobMartinOrderDate = "12/31/2021";
         String actualBobMartinOrderDate = bobMartinDateCell.getText();
-        Assert.assertEquals(actualBobMartinOrderDate,expectedBobMartinOrderDate);
+        Assert.assertEquals(actualBobMartinOrderDate, expectedBobMartinOrderDate);
+    }
 
+    //We use the utility method we created.
+    @Test
+    public void test2() {
+
+        String costumerOrderDate1 = WebTableUtils.returnOrderDate(driver, "Alexandra Gray");
+        System.out.println("costumerOrderDate1 = " + costumerOrderDate1);
+
+
+        String costumerOrderDate2 = WebTableUtils.returnOrderDate(driver, "John Doe");
+        System.out.println("costumerOrderDate2 = " + costumerOrderDate2);
 
     }
 
+    //Using WebTableUtils.orderVerify(); method
+    @Test
+    public void test3() {
+
+        WebTableUtils.orderVerify(driver, "Bart Fisher", "01/16/2021");
+
+    }
 }
 /*
 TC #1: Web table practice
