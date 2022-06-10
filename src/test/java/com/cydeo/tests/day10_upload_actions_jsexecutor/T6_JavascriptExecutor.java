db6585539efaee41d9588d654da13b62820bc475
Elshat <z90602190@hotmail.com>
2022-06-10 23:01:38 +0300

    day11_actions_jsexecutor_practice

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
@@ -15,17 +15,23 @@ public class T6_JavascriptExecutor {
         Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll ");
 
         //3- Use below JavaScript method and scroll
+        //((JavascriptExecutor)Driver.getDriver()).executeScript("");
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //a. 750 pixels down 10 times.
         for (int i = 0; i < 10; i++) {
             BrowserUtils.sleep(1);
             js.executeScript("window.scrollBy(0,750)");
 
         }
 
-        //((JavascriptExecutor)Driver.getDriver()).executeScript("");
 
-        //a. 750 pixels down 10 times.
         //b. 750 pixels up 10 times
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+
+        }
         //JavaScript method to use : window.scrollBy(0,0)
 
     }
