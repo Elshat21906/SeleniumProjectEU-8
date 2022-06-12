d50095ffd7a1332734ccb587c8142be4b92e47b6
Elshat <z90602190@hotmail.com>
2022-06-12 14:10:12 +0300

    day12_pom_design_explicit_wait

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
@@ -36,16 +36,29 @@ public class WebDriveWaitPractices {
     public void dynamic_load_1_test(){
         //1. Go to https://practice.cydeo.com/dynamic_loading/1
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+
         DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
 
         //2. Click to start
         dynamicLoad1Page.startButton.click();
 
         //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+
         //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
+
         //5. Enter username: tomsmith
+        dynamicLoad1Page.inputUsername.sendKeys("tomsmith");
+
         //6. Enter password: incorrectpassword
+        dynamicLoad1Page.inputPassword.sendKeys("asdasdasd");
+
         //7. Click to Submit button
-        //8. Assert “Your password is invalid!” text is displayed.
+        dynamicLoad1Page.submitButton.click();
+
+        //8. Assert “Your password is invalid!” text is displayed
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
     }
 }
