9c55aaee28310a996fbf99b5149e691a5df90b2c
Elshat <z90602190@hotmail.com>
2022-06-12 13:29:35 +0300

    day12_pom_design_explicit_wait

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
@@ -1,8 +1,10 @@
 package com.cydeo.tests.day12_pom_design_explicit_wait;
 
+import com.cydeo.pages.DynamicLoad7Page;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WebDriveWaitPractices {
@@ -18,13 +20,23 @@ public class WebDriveWaitPractices {
         //use the "wait" object to create our expected condition
         wait.until(ExpectedConditions.titleIs("Dynamic title"));
 
+        //Create object of DynamicLoad7Page to reach and assert web elements
+        DynamicLoad7Page dynamicLoad7Page = new DynamicLoad7Page();
+
         //3. Assert: Message “Done” is displayed.
+        Assert.assertTrue(dynamicLoad7Page.doneMessage.isDisplayed());
+
         //4. Assert: Image is displayed
+        Assert.assertTrue(dynamicLoad7Page.spongeBobImage.isDisplayed());
+
+        Driver.closeDriver();
     }
 
     @Test
     public void dynamic_load_1_test(){
         //1. Go to https://practice.cydeo.com/dynamic_loading/1
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+
         //2. Click to start
         //3. Wait until loading bar disappears
         //4. Assert username inputbox is displayed
