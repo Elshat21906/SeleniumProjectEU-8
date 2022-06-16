23dcadecbecf80c2f7596aed9e9876ce4f7a4094
Elshat <z90602190@hotmail.com>
2022-06-16 12:11:32 +0300

    day13_review_and_practices

--- src/test/java/com/cydeo/pages/DynamicControlsPage.java
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -11,9 +11,19 @@ public class DynamicControlsPage {
     }
 
     //@FindBy(xpath = "//button[@type='button']")
-    //@FindBy(xpath = "//button[.='Remove']")
+    //@FindBy(xpath = "//button[.='Remove']")  xpath:text
     @FindBy(css = "form#checkbox-example>button") // id
     public WebElement removeButton;
+
+    @FindBy(css = "div#=loading")
+    public WebElement loadingBar;
+
+    //@FindBy(css = "input[label='blah']")
+    @FindBy(css = "input[type='checkbox']")
+    public WebElement checkBox;
+
+    @FindBy(css = "p[id='message']")
+    public WebElement message;
 
 
 }
