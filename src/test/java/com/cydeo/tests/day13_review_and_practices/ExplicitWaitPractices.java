3f8a0c1d79ca74023956a04ae142dae90cd3727e
Elshat <z90602190@hotmail.com>
2022-06-16 12:28:14 +0300

    day13_review_and_practices

--- src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -1,30 +1,55 @@
 package com.cydeo.tests.day13_review_and_practices;
 
+import com.cydeo.pages.DynamicControlsPage;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ExplicitWaitPractices {
 
+    DynamicControlsPage dynamicControlsPage;
+
     @BeforeMethod
     public void setupMethod(){
         Driver.getDriver().get(" https://practice.cydeo.com/dynamic_controls");
+        dynamicControlsPage = new DynamicControlsPage();
+
     }
 
     @Test
     public void remove_button_test(){
 
+        //3- Click to “Remove” button
+        dynamicControlsPage.removeButton.click();
+
+        //4- Wait until “loading bar disappears”
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+
+        //5- Verify:
+        //a. Checkbox is not displayed
+        Assert.assertTrue(dynamicControlsPage.checkBox.isDisplayed());
+
+        //assertFalse method will pass the test if the boolean value returned is: false
+        Assert.assertFalse(dynamicControlsPage.checkBox.isDisplayed());
+
+        //b. “It’s gone!” message is displayed.
+
     }
-    //TC #2: Explicit wait practice
-    //1- Open a Chrome browser
-    //2- Go to: https://practice.cydeo.com/dynamic_controls
-    //3- Click to “Remove” button
 
-    //4- Wait until “loading bar disappears”
-    //5- Verify:
-    //a. Checkbox is not displayed
-    //b. “It’s gone!” message is displayed.
-
-    //NOTE: FOLLOW POM
 
 }
+//TC #2: Explicit wait practice
+//1- Open a Chrome browser
+//2- Go to: https://practice.cydeo.com/dynamic_controls
+//3- Click to “Remove” button
+
+//4- Wait until “loading bar disappears”
+//5- Verify:
+//a. Checkbox is not displayed
+//b. “It’s gone!” message is displayed.
+
+//NOTE: FOLLOW POM
